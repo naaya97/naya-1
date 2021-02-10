c5803db5db309414f7f91106ed31a84b3376e5de
naya97 <78640234+naya97@users.noreply.github.com>
2021-02-10 11:53:18 +0300

    Add files via upload

--- sms.js
+++ sms.js
@@ -6,15 +6,15 @@ function addtolist(){
     }
 function sendinfo() { 
     let xhttp = new XMLHttpRequest();
-    let mes=document.getElementById("sms");
+    //let mes=document.getElementById("sms");
     let url = '/https://601bf66d1a9c22001706003a.mockapi.io/api/test/sms';
     xhttp.onreadystatechange=function () {  //Call a function when the state changes.
     if (this.readyState ==4 && this.readyState==200) { //means everything is all right.In the same way ajax call works.
-        alert(xhttp.responseText); //returns the text received from a server
+        document.getElementById("sms").innerHTML=this.responseText; //returns the text received from a server
     }
     };
-    xhttp.open('POST', url, true);
-    xhttp.send(mes);
+    xhttp.open('GET', url, true);
+    xhttp.send();
 }
 
 

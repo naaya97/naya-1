3cc8325eb41e3b99a5870570b1bfd5df1f5148eb
naya97 <78640234+naya97@users.noreply.github.com>
2021-03-30 20:04:29 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+
 function addToTable ()
 {
   let phNumber=document.getElementById("phoneNumber").value;
@@ -7,12 +8,57 @@ function addToTable ()
   let cell = newRow.insertCell(0);
   let cell2 = newRow.insertCell(1);
   cell2.innerText=conName+"\n"+phNumber;
-  let element1 = document.createElement("input");
-  element1.type = "checkbox";
-  element1.name="chk";
-  element1.value="";
-  cell.appendChild(element1); 
+  let element = document.createElement("input");
+  element.type = "checkbox";
+  element.name="chk";
+  element.value="";
+  cell.appendChild(element);
+ 
 }
+
+let arr = [];
+function addToArray ()
+{
+  let phNumber=document.getElementById("phoneNumber").value;
+  let conName=document.getElementById("contactName").value;
+  arr.push(conName+phNumber);
+}
+
+/*
+function filterArr() {
+  checkEle.forEach((checkbox) => {
+    if (checkbox.checked) checkboxValues.push(checkbox.value);
+  });
+      }
+*/
+
+
+
+
+/*
+let elementTypesSelected;
+let getElement = element.filter(function (element) {
+    return elementTypesSelected.indexOf(element.storeType) > -1;
+});
+*/
+
+
+/*
+document.addEventListener('change', () => {
+  const checkedValues = [...document.querySelectorAll('.elementCheckBox')]
+    .filter(input => input.checked)
+    .map(input => input.value);
+  const filteredStores = element.filter(({ elementType }) => checkedValues.includes(elementType));
+  console.log(filteredElement);
+});
+*/
+
+
+
+
+
+
+
 
 
 showChecked();
@@ -29,26 +75,26 @@ function showChecked()
      }
    });
 
-   let arr = [];
-   function addToArray ()
-   {
-     let phNumber=document.getElementById("phoneNumber").value;
-     let conName=document.getElementById("contactName").value;
-     arr.push(conName+phNumber);
-   }
- 
+/*
+//Getting all selected checkboxes
+let inputs = document.getElementsByTagName("input");
+let checked = []; //will contain all checked checkboxes
+for (let i = 0; i < inputs.length; i++) { 
+    if (inputs[i].checked) {
+      checked.push(inputs[i]);
+    }
+  }
+let nbChecked = checked.length; //number of checked checkboxes
+document.getElementById("checkValue").innerText="Selected phone numbers are : " + nbChecked +" Numbers";
 
-
-  
-
-
-
+*/
+   
 
    function send() 
 {
   const data=
      {
-  phoneNumbers:arr ,
+  phoneNumbers:  arr,
   messageTemplate: document.getElementById("mesTe").value,
      };       
   const jsonStr = JSON.stringify(data);
@@ -58,3 +104,17 @@ function showChecked()
   xhttp.send(jsonStr);
   console.log(jsonStr);
 }
+
+
+/*
+//hide table
+function myFunction() {
+  document.getElementById("myDIV").style.display = 'none';
+}
+*/
+
+
+
+
+
+
